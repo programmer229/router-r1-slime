f562aa216e5ca54f2f90070a272b9dac7336eea8
programmer229 <ddengalev@gmail.com>
2025-10-13 15:18:45 +0200

    Fix bug

--- router_r1_example/run_router_r1_qwen2.5_3B.sh
+++ router_r1_example/run_router_r1_qwen2.5_3B.sh
@@ -3,13 +3,6 @@
 # Router-R1 training launcher adapted from the Search-R1 slime example.
 
 pkill -9 sglang
-sleep 3
-ray stop --force
-pkill -9 ray
-pkill -9 python
-sleep 3
-pkill -9 ray
-pkill -9 python
 
 set -ex
 
