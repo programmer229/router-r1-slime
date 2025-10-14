827039f6211489e3fe54281ce54f808fefcab1ab
programmer229 <ddengalev@gmail.com>
2025-10-14 13:11:27 +0200

    Fix bug

--- router_r1_example/run_router_r1_qwen2.5_3B.sh
+++ router_r1_example/run_router_r1_qwen2.5_3B.sh
@@ -112,7 +112,7 @@ RUNTIME_ENV_JSON="{
   }
 }"
 
-ray job submit --address="http://127.0.0.1:4242" \
+ray job submit --address="http://10.42.8.245:4242" \
    --runtime-env-json="${RUNTIME_ENV_JSON}" \
    -- python3 slime/train.py \
    --actor-num-nodes 1 \
